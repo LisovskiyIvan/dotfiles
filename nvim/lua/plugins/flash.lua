afa0ca1e2896ad851a93f6ea9129cb151da4f3ed
Lisovskii Ivan <daymedead.dev@gmail.com>
2026-05-15 21:37:26 +0300

    helix jump

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,54 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      search = { enabled = false },
+      char = {
+        enabled = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash jump",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Flash remote",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash treesitter search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle flash search",
+    },
+  },
+}
