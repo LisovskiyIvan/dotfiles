8d17e59e6114aaef1f1e6e60195b3ea6e08f8a39
Lisovskii Ivan <daymedead.dev@gmail.com>
2026-05-24 16:49:03 +0300

    fix font for neovide

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,7 +6,8 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.cursorline = true
 
 if vim.g.neovide then
-  vim.g.neovide_opacity = 0.85
+  vim.g.neovide_opacity = 0.3
+  vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h14"
   vim.g.neovide_floating_blur_amount_x = 3.0
   vim.g.neovide_floating_blur_amount_y = 3.0
   vim.g.neovide_confirm_quit = true
