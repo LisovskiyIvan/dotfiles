0275266fb4a0afd1985877d3889b3aebfcce4792
Lisovskii Ivan <daymedead.dev@gmail.com>
2026-05-27 15:56:14 +0300

    style for nevide

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.cursorline = true
 
 if vim.g.neovide then
   vim.g.neovide_opacity = 0.3
-  vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h14"
+  vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h10"
   vim.g.neovide_floating_blur_amount_x = 3.0
   vim.g.neovide_floating_blur_amount_y = 3.0
   vim.g.neovide_confirm_quit = true
